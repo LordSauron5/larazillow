00ac614f2add6a3813227660ae872873676faa51
Tyron Aricun <tyronaricum1@gmail.com>
2023-04-23 19:20:45 +0200

    add validation

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -35,7 +35,18 @@ class ListingController extends Controller
      */
     public function store(Request $request)
     {
-        Listing::create($request->all());
+        Listing::create(
+                $request->validate([
+                'beds' => 'required|integer|min:0|max:20',
+                'baths' => 'required|integer|min:0|max:20',
+                'area' => 'required|integer|min:15|max:1500',
+                'city' => 'required',
+                'code' => 'required',
+                'street' => 'required',
+                'street_nr' => 'required|integer|min:1|max:1000',
+                'price' => 'required|integer|min:1|max:20000000000',
+            ])
+        );
 
         return redirect()->route('listing.index')
             ->with('success', 'Listing was created successfully!');
--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -2,15 +2,28 @@
     <Link href="/">Main Page</Link>&nbsp;
     <Link href="/hello">Show Page</Link>
     <!-- <div>The Page with timer: {{ timer }}</div> -->
+    <div v-if="success" class="success">
+        {{ success }}
+    </div>
     <slot>Default</slot>
 </template>
 
 <script setup>
-    import { Link } from '@inertiajs/vue3'
-    import { ref } from "vue"
+    import { computed } from 'vue'
+    import { Link, usePage } from '@inertiajs/vue3'
+
+    // page.props.value.flash.success
+    const success = computed(()=> usePage().props.flash.success)
 
     // const timer = ref(0)
     // setInterval(() => {
     //     timer.value++
     // }, 1000);
 </script>
+
+<style scoped>
+    .success {
+        background-color: green;
+        color: white;
+    }
+</style>
