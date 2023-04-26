435cef54b231cd903b4498867afe9e2781734620
Tyron Aricun <tyronaricum1@gmail.com>
2023-04-26 19:03:00 +0200

    updated listing show layout components

--- resources/js/Pages/Listing/Show.vue
+++ resources/js/Pages/Listing/Show.vue
@@ -1,12 +1,38 @@
 <template>
-    <div>
-        <ListingAddress :listing="listing" />
+    <div class="flex flex-col-reverse md:grid grid-cols-12 gap-4">
+        <Box class="md:col-span-7 flex items-center w-full">
+            <div class="w-full text-center font-medium text-gray-500">
+                No images
+            </div>
+        </Box>
+        <div class="md:col-span-5 flex flex-col gap-4">
+            <Box>
+                <template #header>
+                    Basic info
+                </template>
+                <Price :price="listing.price" class="text-2xl font-bold"/>
+                <ListingSpace :listing="listing" class="text-lg"/>
+                <ListingAddress :listing="listing" class="text-gray"/>
+            </Box>
+            <Box>
+                <template #header>
+                    Offer
+                </template>
+                Make an offer
+            </Box>
+        </div>
     </div>
 </template>
 
 <script setup>
-    import ListingAddress from '@/Components/ListingAddress.vue'
-    defineProps({
-        listing: Object
-    })
+
+import ListingAddress from '@/Components/ListingAddress.vue'
+import Box from '../../Components/UI/Box.vue';
+import ListingSpace from '../../Components/UI/ListingSpace.vue';
+import Price from '../../Components/UI/Price.vue';
+
+defineProps({
+    listing: Object
+})
+
 </script>
