0b34f5dab664d7dfd9a126dd57b94c753e1e245b
Tyron Aricum <tyronaricum1@gmail.com>
2023-07-23 22:26:58 +0200

    login/logout reference updates

--- resources/js/Pages/UserAccount/Create.vue
+++ resources/js/Pages/UserAccount/Create.vue
@@ -23,12 +23,15 @@
             <div class="mt-4">
                 <button class="btn-primary w-full" type="submit">Create Account</button>
             </div>
+            <div class="mt-2 text-center">
+                <Link :href="route('login')" class="text-sm text-gray-500">Already have an account? Click here</Link>
+            </div>
         </div>
     </form>
 </template>
 
 <script setup>
-import { useForm } from '@inertiajs/vue3';
+import { useForm, Link } from '@inertiajs/vue3';
 
 const form = useForm({
     name: null,
