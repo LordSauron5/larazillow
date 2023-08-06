6552bda4b2b1996d48b874e32bb2ac659532bebd
Tyron Aricum <tyronaricum1@gmail.com>
2023-08-06 19:21:58 +0200

    update: add js filtering logic

--- resources/js/Pages/Listing/Index.vue
+++ resources/js/Pages/Listing/Index.vue
@@ -1,5 +1,5 @@
 <template>
-    <Filters />
+    <Filters :filters="filters"/>
     <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
         <Listing v-for="listing in listings.data" :key="listing.id" :listing="listing"/>
     </div>
@@ -18,6 +18,7 @@
 
     defineProps({
         listings: Array,
+        filters: Object,
     })
 
 </script>
