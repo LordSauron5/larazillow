9e48581d924d5e7d94fcdfae716bcd53f2a765ca
Tyron Aricum <tyronaricum1@gmail.com>
2024-06-08 12:17:33 +0200

    update: fix minor styling and hide accept button on offers that have been sold

--- resources/js/Pages/Realtor/Show.vue
+++ resources/js/Pages/Realtor/Show.vue
@@ -13,22 +13,24 @@
             </div>
         </Box>
 
-            <div v-else class="md:col-span-7 items-center">
-                <Offer 
-                    v-for="offer in listing.offers" 
-                    :key="offer.id" class="mb-4"
-                    :offer="offer"
-                    :listing-price="listing.price"
-                />
-            </div>
+        <div v-else class="md:col-span-7 flex flex-col gap-4">
+            <Offer 
+                v-for="offer in listing.offers" 
+                :key="offer.id"
+                :offer="offer"
+                :listing-price="listing.price"
+            />
+        </div>
 
-        <Box class="md:col-span-5">
-            <template #header>Basic Info</template>
-            <Price :price="listing.price" class="text-2xl font-bold" />
-
-            <ListingSpace :listing="listing" class="text-lg" />
-            <ListingAddress :listing="listing" class="text-gray-500" />
-        </Box>
+        <div class="md:col-span-5">
+            <Box>
+                <template #header>Basic Info</template>
+                <Price :price="listing.price" class="text-2xl font-bold" />
+    
+                <ListingSpace :listing="listing" class="text-lg" />
+                <ListingAddress :listing="listing" class="text-gray-500" />
+            </Box>
+        </div>
     </section>
 </template>
 
