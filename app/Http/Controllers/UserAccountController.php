1f8cd23ae34d36413eeb3e58a0458b3f19905519
Tyron Aricum <tyronaricum1@gmail.com>
2024-06-13 19:02:44 +0200

    update: add event for when user registers

--- app/Http/Controllers/UserAccountController.php
+++ app/Http/Controllers/UserAccountController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
@@ -27,6 +28,7 @@ class UserAccountController extends Controller
 
         // authenticate the user
         Auth::login($user);
+        event(new Registered($user));
 
         return redirect()->route('listing.index')->with('success', 'Your account has been created');
     }
