75121952d5c5a429af837a77a06aa2726fe7738c
Tyron Aricum <tyronaricum1@gmail.com>
2024-06-13 18:55:38 +0200

    update: apply email sending

--- app/Notifications/OfferMade.php
+++ app/Notifications/OfferMade.php
@@ -28,7 +28,7 @@ class OfferMade extends Notification
      */
     public function via(object $notifiable): array
     {
-        return ['database'];
+        return ['database', 'mail'];
         // return ['mail'];
     }
 
@@ -38,9 +38,9 @@ class OfferMade extends Notification
     public function toMail(object $notifiable): MailMessage
     {
         return (new MailMessage)
-                    ->line('The introduction to the notification.')
-                    ->action('Notification Action', url('/'))
-                    ->line('Thank you for using our application!');
+            ->line("New offer R {$this->offer->amount} was made for your listing.")
+            ->action('See your listng', route('realtor.listing.show', ['listing' => $this->offer->listing_id]))
+            ->line('Thank you for using our application!');
     }
 
     /**
